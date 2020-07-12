2b64527f148ef1d1b33dd1be69544a27e026a7ee
comradeMauser <60297848+comradeMauser@users.noreply.github.com>
2020-07-12 23:13:53 +0300

    task(types)

---
+++ types.js
@@ -0,0 +1,17 @@
+//============ Training  ============
+//============ (learnJavascript) ============
+
+//============ Types
+console.log("O:", typeof 0);
+console.log("'string':", typeof "string");
+console.log("true:", typeof true);
+console.log("[]", typeof []);
+console.log("{}", typeof {});
+console.log("Symbol('Symbol')", typeof Symbol('Symbol'));
+console.log("undefined:", typeof undefined);
+
+console.log("null", typeof null);
+console.log("Math:", typeof Math);
+console.log("10n:", typeof 10n);
+console.log("Map:", typeof Map);
+
