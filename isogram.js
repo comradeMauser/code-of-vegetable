ce9064e41a55bafd4b02d5b9ae779f3088ea857b
comradeMauser <60297848+comradeMauser@users.noreply.github.com>
2020-07-15 22:55:14 +0300

    new solution

--- isogram.js
+++ isogram.js
@@ -7,9 +7,9 @@ Implement a function that determines whether a string that contains only letters
 Assume the empty string is an isogram. Ignore letter case.
 */
 
-function isIsogram(str){
-  let word = str.toLowerCase();
-  let res = new Set(word);
-  return res.size === word.length
+const isIsogram = (str) => {
+    return new Set(str.toLowerCase()).size === str.length
 }
+
+//You have passed all of the tests! :)
 
