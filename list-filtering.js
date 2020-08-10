5b5977808319b2bed4e291064d43ea5f559e20af
comradeMauser <60297848+comradeMauser@users.noreply.github.com>
2020-08-10 20:37:06 +0300

    update

--- list-filtering.js
+++ list-filtering.js
@@ -6,17 +6,9 @@ integers and strings and returns a new list with the strings filtered out.
 */
 
 /*  ===Example===
-filter_list([1,2,'a','b']) == [1,2]
 filter_list([1,'a','b',0,15]) == [1,0,15]
-filter_list([1,2,'aasf','1','123',123]) == [1,2,123]
 */
 
-let list = [1, 2, 'aasf', '1', '123', 123];
-
 function filter_list(list) {
-    return indArr = list.filter(
-        (element) => Number.isInteger(element.valueOf())
-    )
+    return list.filter(element => Number.isInteger(element))
 }
-
-console.debug("result:", filter_list(list));
