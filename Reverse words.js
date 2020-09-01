7dc3042821f795b25e9d04fb3633d22f6a8fd225
comradeMauser <60297848+comradeMauser@users.noreply.github.com>
2020-09-01 23:19:05 +0300

    a try

--- Reverse words.js
+++ Reverse words.js
@@ -1,22 +1,30 @@
+//      Training
+//      www.codewars.com
+
+/*  Instructions
+Complete the function that accepts a string parameter,
+and reverses each word in the string.
+All spaces in the string should be retained.
+*/
+
+/*  Examples
+"This is an example!" ==> "sihT si na !elpmaxe"
+"double  spaces"      ==> "elbuod  secaps"
+*/
 
 
 function reverseWords(str) {
     const array = []
-    let subString = ""
+    let string = " " + str + " "
+    let subString = " "
     let index = 0
-    for (let n = 0; n < str.length; n++) {
-
-        if (str[n] === " ") {
-
-            subString += (str.slice(index, n))
+    for (let n = 0; n < string.length; n++) {
+        if (string[n] === " ") {
+            subString += (string.slice(index, n))
             index = n
-            // console.log(str.slice(index, n))
             array.push(subString.split('').reverse().join(''))
             subString = ""
         }
-
     }
     return array.join('').trim()
 }
-
-reverseWords(str)
