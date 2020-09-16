9ab0f9446e0dc1abf462f101bc051d26fe0f1a2d
comradeMauser <60297848+comradeMauser@users.noreply.github.com>
2020-09-16 18:11:03 +0300

    update

--- Reverse words.js
+++ Reverse words.js
@@ -5,13 +5,11 @@
 Complete the function that accepts a string parameter,
 and reverses each word in the string.
 All spaces in the string should be retained.
-*/
 
-/*  Examples
+    Examples
 "This is an example!" ==> "sihT si na !elpmaxe"
 "double  spaces"      ==> "elbuod  secaps"
 */
-
 
 function reverseWords(str) {
     const array = []
--- Square Every Digit.js
+++ Square Every Digit.js
@@ -4,9 +4,9 @@
 /*  Instructions
 Welcome. In this kata, you are asked to square every digit of a number
 and concatenate them.
-Note: The function accepts an integer and returns an integer */
+Note: The function accepts an integer and returns an integer.
 
-/*  Example
+    Example
 For example, if we run 9119 through the function, 811181 will come out, because 9**2 is 81 and 1**2 is 1.
 */
 
