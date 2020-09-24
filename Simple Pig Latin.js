d71c5cabd18af1688ac435f1e4c11b379f0e6d16
comradeMauser <60297848+comradeMauser@users.noreply.github.com>
2020-09-24 21:49:23 +0300

    update

---
+++ Simple Pig Latin.js
@@ -0,0 +1,19 @@
+//      Training
+//      www.codewars.com
+
+/*  Instructions
+Move the first letter of each word to the end of it, then add "ay" to the end of the word.
+Leave punctuation marks untouched.
+
+    Examples
+'Pig latin is cool' ==> igPay atinlay siay oolcay
+'Hello world !' ==> elloHay orldway !
+*/
+
+function pigIt(str){
+    return str.trim().match(/[a-z]+/ig)
+        .map(word => {
+            word += word[word.length] = word[0] + "ay"
+            return word.slice(1)
+        }).join(" ")
+}
